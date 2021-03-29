5ceb7687c996a89dd84af1baec405e929ce67d93
Admin <admin@Admins-MacBook-Pro.local>
2021-03-29 15:07:42 +0530

    first commit

---
+++ SampleGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleGit
+//
+//  Created by Admin on 29/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
